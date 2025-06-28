f559293f027a3d3e855e60b6ba01407e1b50d58b
Md. Yasin Mollah <mymofcl@gmail.com>
2025-06-29 00:04:17 +0600

    Day 10 completed in 40 days of javascript challenge

--- Day_10/Scripts/index.js
+++ Day_10/Scripts/index.js
@@ -1,2 +1,40 @@
 console.log("Day 10 out of 40 days of javascript");
 
+// 4 types of Scope in javascript
+// 1. Global Scope
+// 2. Function Scope
+// 3. Block Scope
+// 4. Module Scope
+
+// Global Scope
+// Variables declared outside of any function or block scope are in the global scope
+var name = "Yasin";
+
+function greetings(){
+    console.log("Hello", name);
+}
+
+greetings();
+
+console.log("Hello", name); //"Yasin"
+
+// Function Scope
+// Variables declared inside a function are only accessible within that function
+
+function Target(){
+    var tasks = "Learning 40 days of Javascript";
+    console.log(tasks);
+}
+
+Target();
+// console.log(tasks);
+
+// Block Scope
+// Variables declared using let and const inside {} can not be accessed outside the block
+// Var is always a function scope and let & const are always a block scope
+
+{
+    let num = 10;
+    console.log(num);
+}
+console.log(num);
